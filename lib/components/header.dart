eff2e25c7adba3d92f54bbd2fa2bf8bf084047ce
Renz Cole <renz2arjhay@gmail.com>
2026-01-10 17:53:36 +0800

    added header

---
+++ lib/components/header.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SvgPicture.asset("assets/images/flexai.svg", width: 50),
+          Row(
+            children: [
+              Text(
+                "Owner:",
+                style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(width: 5),
+              Text("MrHairy", style: TextStyle(fontFamily: "Poppins")),
+              SizedBox(width: 6),
+              SvgPicture.asset("assets/images/crown.svg", width: 15),
+            ],
+          ),
+          Icon(Icons.menu, size: 30),
+        ],
+      ),
+    );
+  }
+}
