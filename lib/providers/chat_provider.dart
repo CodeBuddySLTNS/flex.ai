69f6ec79cf68c8eee953fb70215ed790434afbf0
Renz Cole <renz2arjhay@gmail.com>
2026-01-16 17:51:26 +0800

    added default ai

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -2,7 +2,11 @@ import 'package:flexai/services/supabase_services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
+final usernameProvider = StateProvider<String>((ref) => '');
 final conversationIdProvider = StateProvider<String>((ref) => '');
+final selectedModelProvider = StateProvider<String>(
+  (ref) => 'bbfb75e2-2a4e-4843-be60-0751440026db',
+);
 
 final chatHistoryProvider = FutureProvider<List<Map<String, dynamic>>>((
   ref,
