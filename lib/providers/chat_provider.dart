e5b238447a9908589de1179e4c248f32d9476914
Renz Cole <renz2arjhay@gmail.com>
2026-01-23 20:06:58 +0800

    fix: add model provider and debug print for chat history fetching

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flexai/services/supabase_services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
 final usernameProvider = StateProvider<String>((ref) => '');
 final conversationIdProvider = StateProvider<String>((ref) => '');
+final modelProvider = StateProvider<String>((ref) => '');
 final selectedModelProvider = StateProvider<String>(
   (ref) => 'bbfb75e2-2a4e-4843-be60-0751440026db',
 );
@@ -11,5 +13,6 @@ final selectedModelProvider = StateProvider<String>(
 final chatHistoryProvider = FutureProvider<List<Map<String, dynamic>>>((
   ref,
 ) async {
+  debugPrint("history fetching...");
   return SupabaseService().getChatHistory();
 });
