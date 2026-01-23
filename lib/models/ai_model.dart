9fcc5125707659ae624dfacdcb62ecc7b5c7f03d
Renz Cole <renz2arjhay@gmail.com>
2026-01-23 20:06:21 +0800

    add optional code parameter to AiModel constructor and fromJson factory

--- lib/models/ai_model.dart
+++ lib/models/ai_model.dart
@@ -2,10 +2,11 @@ class AiModel {
   String id;
   String title;
   String? authorId;
+  String? code;
 
-  AiModel(this.id, this.title, [this.authorId]);
+  AiModel(this.id, this.title, [this.authorId, this.code]);
 
   factory AiModel.fromJson(Map<String, dynamic> json) {
-    return AiModel(json['id'], json['title'], json['author_id']);
+    return AiModel(json['id'], json['title'], json['author_id'], json['code']);
   }
 }
