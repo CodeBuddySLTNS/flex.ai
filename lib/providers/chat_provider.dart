704d0c2663fc0c60bd35da61c86fd1301016a82c
Renz Cole <renz2arjhay@gmail.com>
2026-02-04 18:53:17 +0800

    added default model from user

--- lib/providers/chat_provider.dart
+++ lib/providers/chat_provider.dart
@@ -1,3 +1,5 @@
+import 'package:flexai/main.dart';
+import 'package:flexai/models/ai_model.dart';
 import 'package:flexai/services/supabase_services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,7 +9,9 @@ final usernameProvider = StateProvider<String>((ref) => '');
 final conversationIdProvider = StateProvider<String>((ref) => '');
 final modelProvider = StateProvider<String>((ref) => '');
 final selectedModelProvider = StateProvider<String>(
-  (ref) => 'bbfb75e2-2a4e-4843-be60-0751440026db',
+  (ref) =>
+      prefs.getString('default_model') ??
+      'bbfb75e2-2a4e-4843-be60-0751440026db',
 );
 
 final chatHistoryProvider = FutureProvider<List<Map<String, dynamic>>>((
@@ -15,4 +19,10 @@ final chatHistoryProvider = FutureProvider<List<Map<String, dynamic>>>((
 ) async {
   debugPrint("history fetching...");
   return SupabaseService().getChatHistory();
+});
+
+// ai models provider - can be invalidated to refetch
+final aiModelsProvider = FutureProvider<List<AiModel>>((ref) async {
+  debugPrint("fetching ai models...");
+  return SupabaseService().getModels();
 });
