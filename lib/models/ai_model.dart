1c3ec9180cb1b0479a423ea13ebfb83897f40bd5
Renz Cole <renz2arjhay@gmail.com>
2026-02-05 13:25:00 +0800

    added owner text property

--- lib/models/ai_model.dart
+++ lib/models/ai_model.dart
@@ -3,10 +3,17 @@ class AiModel {
   String title;
   String? authorId;
   String? code;
+  String? ownerText;
 
-  AiModel(this.id, this.title, [this.authorId, this.code]);
+  AiModel(this.id, this.title, [this.authorId, this.code, this.ownerText]);
 
   factory AiModel.fromJson(Map<String, dynamic> json) {
-    return AiModel(json['id'], json['title'], json['author_id'], json['code']);
+    return AiModel(
+      json['id'],
+      json['title'],
+      json['author_id'],
+      json['code'],
+      json['owner_text'],
+    );
   }
 }
